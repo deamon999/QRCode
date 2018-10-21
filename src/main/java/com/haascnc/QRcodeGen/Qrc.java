34378c674841254e6c3e591f5670264a746bdcaa
Dmytro Ileiko <deamon999@gmail.com>
2018-10-21 19:17:01 +0200

    pdf write posibility

--- src/main/java/com/haascnc/QRcodeGen/Qrc.java
+++ src/main/java/com/haascnc/QRcodeGen/Qrc.java
@@ -1,12 +1,15 @@
 package com.haascnc.QRcodeGen;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
 import net.glxn.qrgen.javase.QRCode;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 
 public class Qrc extends JFrame implements ActionListener {
     private JLabel label;
@@ -19,6 +22,7 @@ public class Qrc extends JFrame implements ActionListener {
     private JPanel workPanel;
 
     private JButton button;
+    private JButton buttonPdf;
 
     public Qrc() {
         setTitle("QRCode Application");
@@ -29,15 +33,17 @@ public class Qrc extends JFrame implements ActionListener {
         //
         labelTwo = new JLabel("Phone info");
         textToQrCodeTwo = new JTextField();
-        textToQrCodeTwo.setPreferredSize(new Dimension(100, 28));
+        textToQrCodeTwo.setPreferredSize(new Dimension(90, 28));
         //
         showQrCode = new JLabel();
         showQrCode.setPreferredSize(new Dimension(640, 450));
         workPanel = new JPanel();
         textToQrCode = new JTextField();
-        textToQrCode.setPreferredSize(new Dimension(100, 28));
+        textToQrCode.setPreferredSize(new Dimension(90, 28));
         button = new JButton("Convert");
+        buttonPdf = new JButton("Write PDF");
         button.addActionListener(this);
+        buttonPdf.addActionListener(this);
 
 
         workPanel.setLayout(new FlowLayout());
@@ -48,6 +54,7 @@ public class Qrc extends JFrame implements ActionListener {
         workPanel.add(textToQrCodeTwo);
         //
         workPanel.add(button);
+        workPanel.add(buttonPdf);
         Container container = getContentPane();
         container.setLayout(new BorderLayout());
         container.add(workPanel, BorderLayout.NORTH);
@@ -63,6 +70,45 @@ public class Qrc extends JFrame implements ActionListener {
             if (text.length() > 0 && phone.length() > 0) {
                 ByteArrayOutputStream stream = QRCode.from(user.toString()).withSize(640, 450).stream();
                 showQrCode.setIcon(new ImageIcon(stream.toByteArray()));
+            }
+        }
+        if (source == buttonPdf) {
+            String text = textToQrCode.getText();
+            String phone = textToQrCodeTwo.getText();
+            User user = new User(text, phone);
+            if (text.length() > 0 && phone.length() > 0) {
+                JFileChooser jFileChooser = new JFileChooser();
+                int result = jFileChooser.showSaveDialog(this);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = jFileChooser.getSelectedFile();
+                    Document document = new Document();
+                    try {
+                        PdfWriter.getInstance(document, new FileOutputStream(selectedFile.toString() + ".pdf"));
+                    } catch (DocumentException e1) {
+                        e1.printStackTrace();
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                    document.open();
+                    document.newPage();
+                    Image image = null;
+                    try {
+                        image = Image.getInstance(QRCode.from(user.toString()).stream().toByteArray());
+                    } catch (BadElementException e1) {
+                        e1.printStackTrace();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    image.setAbsolutePosition(0, 0);
+                    image.setBorderWidth(0);
+                    image.scaleAbsolute(PageSize.A4);
+                    try {
+                        document.add(image);
+                    } catch (DocumentException e1) {
+                        e1.printStackTrace();
+                    }
+                    document.close();
+                }
             }
         }
     }
